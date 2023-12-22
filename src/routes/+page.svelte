9a20a8083a80fd4fcc7c007072a689fb521c9e0d
ndrooo <git@ndr.ooo>
2023-12-22 13:33:02 -0500

    random email prefix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,33 @@
   import Header from '$lib/Header.svelte';
   import NavLink from '$lib/NavLink.svelte';
   import SrcLink from '$lib/SrcLink.svelte';
+  import { onMount } from 'svelte';
+
+  let emailPrefix = 'a';
+  let prefixes = [
+    'a',
+    'i-dont-understa',
+    'on-the-other-ha',
+    'in-dema',
+    'protected-wetla',
+    'a-bit-bla',
+    'land-and-expa',
+    'pituitary-gla',
+    'death-stra',
+    'i-cant-sta',
+    'i-dont-like-sa',
+    'aint-it-gra',
+    'cobra-comma',
+    'ampersa',
+    'wayla',
+    'fools-erra',
+    'were-an-american-ba',
+    'inline-spa'
+  ];
+
+  onMount(() => {
+    emailPrefix = prefixes[Math.floor(Math.random() * prefixes.length)];
+  });
 </script>
 
 <Header title="a.ndr.ooo" blurb="This is my website">
@@ -14,5 +41,5 @@
   to put here or in my github, whoops. I also like playing around with unix and
   building my <a href="https://github.com/ndrooo/dot">NixOS configuration</a>. I
   live in New York City and I think birds are cool. You can email me at
-  <code>^[\w-\.]+@ndr\.ooo$</code>.
+  <a href="mailto:{emailPrefix}@ndr.ooo">{emailPrefix}@ndr.ooo</a>.
 </p>
