e6a1dde5589b75076699e3726a9b3b4e17c98d1b
ndrooo <git@ndr.ooo>
2025-04-30 15:58:51 -0400

    Home page content tweaks

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,16 @@
+<svelte:head>
+  <title>a.ndr.ooo</title>
+</svelte:head>
+
 <h1>a.ndr.ooo</h1>
 
 <p>This is an intro section</p>
-<p>Look at my <a href="/web-a11y-audit">web accessibility audit!</a></p>
+<p>Look at my <a href="/web-a11y-audit">web accessibility audit</a>!</p>
 
 <h2>about</h2>
-<p>I'm <b>Andrew</b>, a software developer and curious person in New York City. To make money I build <b>websites</b> and I am particularly focused on making them <b>accessible</b>. When I feel like writing software for fun I build <b>additional websites</b>, dabble in <b>game development</b>, mess around with <b>Unix</b>, and continuously improve my <a href="https://github.com/ndrooo/nix">NixOS configuration</a>.</p>
-<p>I am a <b>gardener</b> of the home and community variety, I'm a <b>birder</b>, and I enjoy learning anything I can about <b>ecology</b>. I like <b>crosswords</b> and other puzzles, and I'm trying to learn how to make them. I do a little bit of <b>electronics and fabrication</b>. I'm very interested in <b>home electrification</b> and <b>climate tech</b> as well.</p>
+<p>I'm <b>Andrew</b>, a software developer and curious person in New York City. To make money I build <b>websites</b> and I am particularly focused on making them <b>accessible</b>. When I feel like writing software for fun I build <b>different websites</b>, dabble in <b>game development</b>, mess around with <b>Unix</b>, and continuously improve my <a href="https://github.com/ndrooo/nix">NixOS configuration</a>.</p>
+<p>I am a <b>gardener</b> of the home and community variety, I'm a <b>birder</b>, and I enjoy learning anything I can about <b>ecology</b>. I like <b>crosswords</b> and other puzzles, and I'm trying to learn how to make them. I dabble in <b>electronics and fabrication</b>. I'm very interested in <b>home electrification</b> and <b>climate tech</b> as well.</p>
+<p>You can contact me at <a href="mailto:a@ndr.ooo">a@ndr.ooo</a>.</p>
 
 <h2>writing</h2>
 <p>Here are links to some other stuff</p>
