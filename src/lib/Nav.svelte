80c1bdbea0dfb839017349a7b2028312d47c02da
ndrooo <git@ndr.ooo>
2025-04-30 15:58:32 -0400

    aria-current in nav

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -1,4 +1,8 @@
+<script lang="ts">
+	import { page } from '$app/state';
+</script>
+
 <nav>
-    <a href="/">go-home</a>
-    <a href="https://github.com/ndrooo">view-source</a>
+	<a href="/" aria-current={page.url.pathname == '/' ? 'page' : undefined}>go-home</a>
+	<a href="https://github.com/ndrooo">view-source</a>
 </nav>
