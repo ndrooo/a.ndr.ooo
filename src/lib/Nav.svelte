6bb3ed6304606b5b13546f157e5325c9e4c2b873
ndrooo <git@ndr.ooo>
2025-04-30 16:32:39 -0400

    Fix source link

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -4,5 +4,5 @@
 
 <nav>
 	<a href="/" aria-current={page.url.pathname == '/' ? 'page' : undefined}>go-home</a>
-	<a href="https://github.com/ndrooo">view-source</a>
+	<a href="https://github.com/ndrooo/a.ndr.ooo">view-source</a>
 </nav>
