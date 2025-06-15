e6acc0c7b73daff0aae979d85c50cfdf3ad37186
ndrooo <git@ndr.ooo>
2025-06-14 21:51:56 -0400

    Main page copy edits

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,16 +4,16 @@
 
 <h1>a.ndr.ooo</h1>
 
-<p>Welcome to my web page! Here are some parts of it you might be interested in:</p>
+<p>Welcome to my web page! Here are some links:</p>
 <ul class="lines-list">
-  <li>Some <a href="website-stuff">website-related resources</a> I've collected, aimed at people making personal or hobby websites for fun</li>
-  <li><b>(Under construction)</b> <a href="/a11y-audit">A simple web accessibility audit</a> for beginners to catch the biggest access issues in your (or any) website</li>
+  <li>Some <a href="website-stuff">website-related resources</a> I've collected for people making personal or hobby websites</li>
+  <li><b>(Under construction)</b> A <a href="/a11y-audit">web accessibility audit</a> for beginners, to catch the biggest access issues in your website</li>
 </ul>
 
 <h2>about me</h2>
 <p>I'm <b>Andrew</b>, a software developer and curious person in New York City. For my job I build <b>websites</b> and I am particularly focused on making them <b>accessible</b>. When I feel like writing software for fun I build <b>different websites</b>, dabble in <b>game development</b>, mess around with <b>Unix</b>, and continuously improve my <a href="https://github.com/ndrooo/nix">NixOS configuration</a>.</p>
-<p>I am a <b>gardener</b> of the home and community variety, I'm a <b>birder</b>, and I enjoy learning anything I can about <b>ecology</b>. I know an immense amount about <b>tea</b> and I do like to drink it sometimes as well. I like <b>crosswords</b> and other puzzles, and I'm trying to learn how to make them. I dabble in <b>electronics and fabrication</b>. I'm very interested in <b>home electrification</b> and <b>climate tech</b> as well.</p>
+<p>I am a <b>gardener</b> of the home and community variety, I'm a <b>birder</b>, and I enjoy learning anything I can about <b>ecology</b>. I know a lot about <b>tea</b> and I do like to drink it as well. I like <b>crosswords</b> and other puzzles, and I'm trying to learn how to make them. I dabble in <b>electronics and fabrication</b>. I'm very interested in <b>home electrification</b> and <b>climate tech</b> as well.</p>
 <p>You can contact me at <a href="mailto:a@ndr.ooo">a@ndr.ooo</a>.</p>
 
 <h2>colophon</h2>
-<p>This <b>website</b> is built with <a href="https://svelte.dev/">Svelte</a>, <a href="https://svelte.dev/docs/kit/introduction">SvelteKit</a>, and <a href="https://mdsvex.pngwn.io/">mdsvex</a>. The <b>source code</b> is available on <a href="https://github.com/ndrooo/a.ndr.ooo">GitHub</a> and the page is hosted on <a href="https://pages.cloudflare.com/">Cloudflare Pages</a>. The <b>font</b> is <a href="https://www.brailleinstitute.org/freefont/">Atkinson Hyperlegible</a>. This bit down here is a <a href="https://indieweb.org/colophon">colophon</a> which explains how the website is made in case you want to <b>build something similar</b>. You can feel free to <b>copy</b> whatever you want because it's all <a href="https://github.com/ndrooo/a.ndr.ooo/blob/main/LICENSE">MIT licensed</a>.</p>
+<p>This <b>website</b> is built with <a href="https://svelte.dev/">Svelte</a>, <a href="https://svelte.dev/docs/kit/introduction">SvelteKit</a>, and <a href="https://mdsvex.pngwn.io/">mdsvex</a>. The <b>source code</b> is available on <a href="https://github.com/ndrooo/a.ndr.ooo">GitHub</a> and the page is hosted on <a href="https://pages.cloudflare.com/">Cloudflare Pages</a>. The <b>font</b> is <a href="https://www.brailleinstitute.org/freefont/">Atkinson Hyperlegible</a>. This is a <a href="https://indieweb.org/colophon">colophon</a>, which explains how the website is made in case you want to <b>build something similar</b>. You can <b>copy</b> whatever you want because it's all <a href="https://github.com/ndrooo/a.ndr.ooo/blob/main/LICENSE">MIT licensed</a>.</p>
