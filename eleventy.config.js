9b74d601aae0018ae6f9d45d9222f4ac2c453838
ndrooo <git@ndr.ooo>
2025-07-26 14:47:05 -0400

    Add favicon

--- eleventy.config.js
+++ eleventy.config.js
@@ -7,6 +7,7 @@ export default function (eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy("./src/css/");
   eleventyConfig.addWatchTarget("./src/css/");
+  eleventyConfig.addPassthroughCopy({"./static/*": "/"});
 
   return {
     dir: {
