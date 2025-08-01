478de318c3e5146e4531eeee87e6f37e4cca525e
ndrooo <git@ndr.ooo>
2025-08-01 00:47:08 -0400

    Use YAML!

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,9 +1,12 @@
 import pluginWebc from "@11ty/eleventy-plugin-webc";
+import YAML from "yaml";
 
 export default function (eleventyConfig) {
   eleventyConfig.addPlugin(pluginWebc, {
     components: "components/**/*.webc",
   });
+
+  eleventyConfig.addDataExtension("yaml", (contents) => YAML.parse(contents));
 
   eleventyConfig.addPassthroughCopy("./css/");
   eleventyConfig.addWatchTarget("./css/");
