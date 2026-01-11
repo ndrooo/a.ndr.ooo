9f675e98ee31902d445b7893801fbea6c6eb73b7
ndrooo <git@ndr.ooo>
2026-01-10 22:30:54 -0500

    Add syntax highlighting with Prism

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,10 +1,12 @@
 import pluginWebc from "@11ty/eleventy-plugin-webc";
+import syntaxHighlight from "@11ty/eleventy-plugin-syntaxhighlight";
 import YAML from "yaml";
 
 export default function (eleventyConfig) {
   eleventyConfig.addPlugin(pluginWebc, {
     components: "components/**/*.webc",
   });
+  eleventyConfig.addPlugin(syntaxHighlight);
 
   eleventyConfig.addDataExtension("yaml", (contents) => YAML.parse(contents));
 
